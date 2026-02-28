9a1a6cd771d780c371a7b40aceadc8617cd3ef20
Bishal Biswas <educatedfool47@gmail.com>
2026-02-28 17:06:27 +0530

    Update sitemap integration to filter admin pages and set site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site : "https://smmpanels.net/",
   output: 'server',
-  integrations: [sitemap()],
+  integrations: [sitemap({
+    filter: (page) => !page.startsWith('https://smmpanels.net/admin/')
+  })],
   server: {
     host: true // Or set this to '0.0.0.0'
   },
